44a9b195b61bf5f45fac2b419f388285df439e4b
mankobe <2511345030@qq.com>
2024-06-23 12:19:27 +0800

    main

---
+++ omni_a51x.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from a51x device
+$(call inherit-product, device/samsung/a51x/device.mk)
+
+PRODUCT_DEVICE := a51x
+PRODUCT_NAME := omni_a51x
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-A5160
+PRODUCT_MANUFACTURER := samsung
+
+PRODUCT_GMS_CLIENTID_BASE := android-samsung-ss
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="a51xzc-user 13 TP1A.220624.014 A5160ZCS7FXA1 release-keys"
+
+BUILD_FINGERPRINT := samsung/a51xzc/a51x:13/TP1A.220624.014/A5160ZCS7FXA1:user/release-keys
